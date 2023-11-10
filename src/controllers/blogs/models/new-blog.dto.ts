a61e8eb810bf608c04186adb255ed483ff8d9c1f
i.stupchyk <i.stupchyk@godeltech.com>
2023-11-10 17:54:36 +0100

    additional validation

--- src/controllers/blogs/models/new-blog.dto.ts
+++ src/controllers/blogs/models/new-blog.dto.ts
@@ -1,12 +1,15 @@
 import { IsString, Length, Matches } from 'class-validator';
+import { IsNotEmptyString } from '../../../utils/validators/check-empty-string.validator';
 
 export class NewBlogDto {
   @Length(1, 15)
   @IsString()
+  @IsNotEmptyString()
   name: string;
 
   @Length(1, 500)
   @IsString()
+  @IsNotEmptyString()
   description: string;
 
   @Length(0, 100)
--- src/controllers/posts/models/new-post.dto.ts
+++ src/controllers/posts/models/new-post.dto.ts
@@ -1,14 +1,21 @@
 import { ObjectId } from 'mongodb';
-import { IsMongoId, Length } from 'class-validator';
+import { IsMongoId, IsString, Length } from 'class-validator';
+import { IsNotEmptyString } from '../../../utils/validators/check-empty-string.validator';
 
 export class NewPostDto {
   @Length(1, 30)
+  @IsString()
+  @IsNotEmptyString()
   title: string;
 
   @Length(1, 100)
+  @IsString()
+  @IsNotEmptyString()
   shortDescription: string;
 
   @Length(1, 1000)
+  @IsString()
+  @IsNotEmptyString()
   content: string;
 
   @IsMongoId()
