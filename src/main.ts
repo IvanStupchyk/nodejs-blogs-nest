7e8a7b43f9db7487fe28083955a143fc09fcae81
ivanstupchyk <ivan.stupchyk@easyfundraising.org.uk>
2024-01-11 11:52:03 +0100

    local swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { appSettings } from './app.settings';
 import * as process from 'process';
 import { TelegramAdapter } from './infrastructure/telegram/telegram.adapter';
 import { connectToNgrok } from './utils/connect-t-ngrok';
+import { swaggerLocal } from './utils/swagger.local';
 
 let appBaseUrl = process.env.APP_BASE_URL || 'http://localhost:3000/';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   appSettings(app);
+
+  swaggerLocal(app);
+
   await app.listen(3000);
 
   const telegramAdapter = await app.resolve(TelegramAdapter);
